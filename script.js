994765a9a9c011747d99242cd7bf20e270537edd
Szafinho89 <szafraniak.tomasz@gmail.com>
2022-10-20 14:10:40 +0200

    version 2.0

--- script.js
+++ script.js
@@ -6,100 +6,84 @@ const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
-//walidator maila
-//  /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i
 
+const clearInputs = (input) => {
+        input.value = ''
+}
 
-//ta funkcja bedzie wyswietlala blad ze jakies pole jest puste
+const clearErrors = (input) => {
+    const formBox = input.parentElement
+    formBox.classList.remove('error')
+}
+
 const showError = (input, msg) => {
     const formBox = input.parentElement
     formBox.classList.add('error')
-    const errorMsg = input.nextElementSibling //lub formBox.qs('.error-text')
-    errorMsg.textContent= msg
-
-    // argument input przechowuje każdego naszego inputa, który miał value='' (chyba)
-    // argument msg przechowuje placeholdery
+    const errorMsg = input.nextElementSibling
+    errorMsg.textContent = msg
 }
 
-
-//ta funkcja bedzie kasowac bledy
-const clearError = (input) => {
-    const formBox = input.parentElement
-    formBox.classList.remove('error') 
-}
-
-
-// ta funkcja bedzie sprawdzala formularz czy wszystkie pola sa wypelnione
 const checkForm = (input) => {
     input.forEach(el => {
         if(el.value === '') {
             showError(el, el.placeholder)
         } else {
-            clearError(el)
+            clearErrors(el)
         }
     })
 }
-//argument INPUT z funkcji checkForm przechowuje tablicę z naszymi inputami
-// argument EL odnosi sie do kazdej zmiennej, ktora umiescilismy w tablicy
 
-
-const checkLength = (input, min) => {
-    if(input.value.length < min) {
-        showError(input, `${input.previousElementSibling.textContent.slice(0, -1)} składa się z min. ${min} znaków.`)
- 
+const checkLength = (input, msg) => {
+    if(input.value.length < msg) {
+        showError(input, `${input.previousElementSibling.textContent.slice(0, -1)} składa się z min ${msg} znaków`)
     }
-    
 }
-
-const checkPassword = (pass1, pass2) => {
-    if(pass1.value !== pass2.value) {
+// tu jeszcze mozna dolozyc RegExpa - podzialac na wyrazeniach regularnych - 
+const checkPasses = (pass1, pass2) => {
+    if(pass2.value !== pass1.value) {
         showError(pass2, `Hasło musi być takie samo w obu polach`)
     }
 }
 
-//ta funkcja bedzie sprawdzala email
-
+// sprobowac z match()
 const checkEmail = (email) => {
-    const regexp = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/;
+    const regexp = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i;
     if (regexp.test(email.value)) {
-        clearError(email)
+        clearErrors(email)
     } else {
-        showError(email, `Niepoprawny email`);
+        showError(email, `Błędny adres email`)
     }
 }
 
-//na koniec funkcja ktora sprawdzi czy mozemy wyslac ten formularz - sprawdzi czy ilosc błędów jest równa 0 i wtedy pokaze nam popup'a
+//sprobowac tu jeszcze raz z matches!!
 const checkErrors = () => {
-    const formBox = document.querySelectorAll('.form-box')
-    let errorCount = 0;
-    formBox.forEach(el => {
-        if(el.classList.contains('error')) {
-            errorCount++
-        };
+    let errorsCounter = 0;
+    const formBoxErrors = document.querySelectorAll('.form-box')
+    formBoxErrors.forEach(el => {
+        if (el.classList.contains('error')) {
+            errorsCounter++
+        }
     })
-
-    if(errorCount === 0) {
+    if (errorsCounter === 0) {
         popup.classList.add('show-popup')
     }
 }
 
-sendBtn.addEventListener('click', (e) => {
-    e.preventDefault()
+sendBtn.addEventListener('click', (event)  => {
+    event.preventDefault();
     checkForm([username, pass, pass2, email])
     checkLength(username, 3)
-    checkLength(password, 8)
-    checkPassword(pass, pass2)
+    checkLength(pass, 8)
+    checkPasses(pass, pass2)
     checkEmail(email)
     checkErrors()
-
 })
 
-//ta funkcja czysci formularz ze wszystkiego co wpisalismy i z błędów
-clearBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    [username, pass, pass2, email].forEach(el => {
-        el.value = ''
-        clearError(el)
+
+clearBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    [username, pass, pass2, email].forEach(input => {
+        clearInputs(input);
+        clearErrors(input);
     })
-    
 })
