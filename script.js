630e67a74d18c93f04fd20df4e7c091d075f6e77
Szafinho89 <szafraniak.tomasz@gmail.com>
2022-10-20 09:39:15 +0200

    1st Version Completed

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const sendBtn = document.querySelector('.send')
 const clearBtn = document.querySelector('.clear')
 const popup = document.querySelector('.popup')
 
-const label = document.querySelector('label')
-console.log(label.textContent);
+//walidator maila
+//  /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i
 
 
 //ta funkcja bedzie wyswietlala blad ze jakies pole jest puste
@@ -34,9 +34,6 @@ const checkForm = (input) => {
     input.forEach(el => {
         if(el.value === '') {
             showError(el, el.placeholder)
-            // const p = el.closest('.error-text')
-            // p.classList.add('error')
-            // console.log('---');
         } else {
             clearError(el)
         }
@@ -56,7 +53,33 @@ const checkLength = (input, min) => {
 
 const checkPassword = (pass1, pass2) => {
     if(pass1.value !== pass2.value) {
-        showError(pass2, `Hało musi być takie samo w obu polach`)
+        showError(pass2, `Hasło musi być takie samo w obu polach`)
+    }
+}
+
+//ta funkcja bedzie sprawdzala email
+
+const checkEmail = (email) => {
+    const regexp = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/;
+    if (regexp.test(email.value)) {
+        clearError(email)
+    } else {
+        showError(email, `Niepoprawny email`);
+    }
+}
+
+//na koniec funkcja ktora sprawdzi czy mozemy wyslac ten formularz - sprawdzi czy ilosc błędów jest równa 0 i wtedy pokaze nam popup'a
+const checkErrors = () => {
+    const formBox = document.querySelectorAll('.form-box')
+    let errorCount = 0;
+    formBox.forEach(el => {
+        if(el.classList.contains('error')) {
+            errorCount++
+        };
+    })
+
+    if(errorCount === 0) {
+        popup.classList.add('show-popup')
     }
 }
 
@@ -66,6 +89,8 @@ sendBtn.addEventListener('click', (e) => {
     checkLength(username, 3)
     checkLength(password, 8)
     checkPassword(pass, pass2)
+    checkEmail(email)
+    checkErrors()
 
 })
 
@@ -74,7 +99,7 @@ clearBtn.addEventListener('click', (e) => {
     e.preventDefault();
     [username, pass, pass2, email].forEach(el => {
         el.value = ''
-        const formBox = el.parentElement
-        formBox.classList.remove('error')
+        clearError(el)
     })
+    
 })
